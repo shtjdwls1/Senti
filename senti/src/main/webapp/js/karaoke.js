45437ada70a1c8a37cf391b2a46735ad3e2bcbc8
mynameisleesiwon <seoil1221@gmail.com>
2022-06-12 11:42:31 +0900

    siwon

---
+++ senti/src/main/webapp/js/karaoke.js
@@ -0,0 +1,112 @@
+const colors = ["#4e4376", "#2b5876"];
+const music = document.getElementById("music");
+const lyrics = document.getElementById("lyrics");
+
+function topColor() {
+	const color1 = colors[0];
+	const color2 = colors[1];
+	button.style.background = `linear-gradient(${color1},${color2})`;
+}
+
+const button = document.querySelector("#top");
+topColor();
+
+
+// 마이크 녹음
+const record = document.getElementById("record");
+const stop = document.getElementById("stop");
+const soundClips = document.getElementById("sound-clips");
+const chkHearMic = document.getElementById("chk-hear-mic");
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)() // 오디오 컨텍스트 정의
+
+const analyser = audioCtx.createAnalyser()
+//        const distortion = audioCtx.createWaveShaper()
+//        const gainNode = audioCtx.createGain()
+//        const biquadFilter = audioCtx.createBiquadFilter()
+
+function makeSound(stream) {
+	const source = audioCtx.createMediaStreamSource(stream)
+
+	source.connect(analyser)
+	//            analyser.connect(distortion)
+	//            distortion.connect(biquadFilter)
+	//            biquadFilter.connect(gainNode)
+	//            gainNode.connect(audioCtx.destination) // connecting the different audio graph nodes together
+	analyser.connect(audioCtx.destination)
+
+}
+
+if (navigator.mediaDevices) {
+	console.log('getUserMedia supported.')
+
+	const constraints = {
+		audio: true
+	}
+	let chunks = []
+
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then(stream => {
+
+			const mediaRecorder = new MediaRecorder(stream)
+
+			record.onclick = () => {
+				mediaRecorder.start()
+				audioCtx.resume()
+				makeSound(stream)
+				console.log(mediaRecorder.state)
+				console.log("recorder started")
+				music.play()
+				lyrics.classList.remove("hidden")
+			}
+
+			stop.onclick = () => {
+				mediaRecorder.stop()
+				audioCtx.suspend()
+				console.log(mediaRecorder.state)
+				console.log("recorder stopped")
+			}
+
+			mediaRecorder.onstop = e => {
+				console.log("data available after MediaRecorder.stop() called.")
+
+				const clipName = prompt("오디오 파일 제목을 입력하세요.", new Date())
+
+				const clipContainer = document.createElement('article')
+				const clipLabel = document.createElement('p')
+				const audio = document.createElement('audio')
+				const deleteButton = document.createElement('button')
+
+				clipContainer.classList.add('clip')
+				audio.setAttribute('controls', '')
+				deleteButton.innerHTML = "삭제"
+				clipLabel.innerHTML = clipName
+
+				clipContainer.appendChild(audio)
+				clipContainer.appendChild(clipLabel)
+				clipContainer.appendChild(deleteButton)
+				soundClips.appendChild(clipContainer)
+
+				audio.controls = true
+				const blob = new Blob(chunks, {
+					'type': 'audio/wav codecs=opus'
+				})
+				chunks = []
+				const audioURL = URL.createObjectURL(blob)
+				audio.src = audioURL
+				console.log("recorder stopped")
+
+				deleteButton.onclick = e => {
+					evtTgt = e.target
+					evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode)
+				}
+			}
+
+			mediaRecorder.ondataavailable = e => {
+				chunks.push(e.data)
+			}
+		})
+		.catch(err => {
+			console.log('The following error occurred: ' + err)
+		})
+}
